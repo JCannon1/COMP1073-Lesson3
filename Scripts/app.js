66dba444c3d96aa4686fe913c0de1e9c8de61961
JCannon1 <jessecannon1@hotmail.com>
2017-01-25 15:46:10 -0500

    Added text and css

--- Scripts/app.js
+++ Scripts/app.js
@@ -2,6 +2,43 @@
 
 console.log("App Started");
 
+// declare / initialize firstHeading variable
+// firstHeading variable creates a link (reference) to the H1 Element
+var firstHeading = document.getElementById("firstHeading");
+
+firstHeading.style.color = "#000000";
+firstHeading.style.fontWeight = "500";
+firstHeading.style.fontStyle = "italic";
+firstHeading.style.opacity = "0.5";
+
+// I can use my firstHeading variable as if it IS the Element 
+// DON'T USE THIS
+// window.alert(firstHeading.textContent); 
+
+// DON't DO THIS EITHER
+// document.write(firstHeading.textContent);
+
+console.log(firstHeading.textContent);
+
+// 3 steps for injecting content onto your page
+// STEP 1 - create a reference to an Element (reference variable)
+var FirstParagraph = document.getElementById("FirstParagraph");
+
+// STEP 2 - create a variable that contains your content (content variable)
+var myContent = "It was a sunny day in Florida. It felt great. I love the sun.";
+
+// STEP 3- assign the variable with your content to the 
+// text content property of the reference variable
+FirstParagraph.textContent = myContent;
+
+var SecondParagraph = document.getElementById("SecondParagraph");
+
+var myHTMLContent = "<h2>Second Heading</h2>"
+                    + " <p>This is an inner paragraph to the Second Paragraph</p>;"
+
+SecondParagraph.innerHTML = myHTMLContent;
+
+
 // create a reference to the button on the page (index.html)
 var clickMeButton = document.getElementById("clickMeButton");
 
