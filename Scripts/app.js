53df40c7ec1eea0c26389180e1beaf0cb7a59e8a
JCannon1 <jessecannon1@hotmail.com>
2017-01-25 12:20:03 -0500

    initial Commit

---
+++ Scripts/app.js
@@ -0,0 +1,14 @@
+/* javascript lives here */
+
+console.log("App Started");
+
+// create a reference to the button on the page (index.html)
+var clickMeButton = document.getElementById("clickMeButton");
+
+// add an event listener - for the click event and call the Click function
+clickMeButton.addEventListener("click", Click)
+
+// click function - used as an event handler 
+function Click() {
+console.log("clicked!");
+}
